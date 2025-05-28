6622619d1e84987477045aaed323aa4d1e2da560
Noctenz <73575503+Noctenz@users.noreply.github.com>
2025-05-28 10:34:44 +0700

    Update

--- script.js
+++ script.js
@@ -16,3 +16,10 @@ document.addEventListener('DOMContentLoaded', function() {
             behavior: 'smooth'
         });
     });
+    
+    const menuBtn = document.querySelector(".menu-btn");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuBtn.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
